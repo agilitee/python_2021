b72082e847f8616205bed3226950078bb158d293
agilitee <74330110+agilitee@users.noreply.github.com>
2021-12-12 16:59:39 +0300

    Delete pythonLab_3 directory

--- pythonLab_3/first/main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from PIL import Image
-
-for i in range(1, 4):
-    img = Image.open('lunar0' + str(i) + '_raw.jpg')
-    data = np.array(img)
-    updated_data = (data - data.min()) * int(255/(data - data.min()).max())
-    res_img = Image.fromarray(updated_data)
-    res_img.save('upd_' + str(i) + '.jpg')
